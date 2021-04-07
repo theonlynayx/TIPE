9ba5c59160b48de7f55f55b3285fbf7d6459c9ce
NudistBeaaach <57132297+NudistBeaaach@users.noreply.github.com>
2021-04-07 20:14:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from OSINT.GetInsta import main
-from tkinter import *
 
 if __name__ == "__main__":
+    
+    # Sacha, la fonction loading c'est une fonction que tu vas implé dans ta GUI et tu vas ensuite la ranger
+    # dans le fichier utils.py à la place de celle que j'ai mis qui pour l'isntant attend juste en ne faisant
+    # rien d'autre.
+
     data = main(input("Username:"))
+
+    # Exemple de comment tu accèderas aux données que je te renvoie
+    
+    for i in data['Stats']['TaggedUsers']:
+        print(i)
